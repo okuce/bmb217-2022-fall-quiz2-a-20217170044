8c76904a3e6e7090ac25969af41dd5aa147b81ed
20217170044 <114662130+20217170044@users.noreply.github.com>
2022-10-06 14:25:52 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,10 +1,15 @@
 class Otobus:
     """Otobus bilet satis takip sinifi"""
-    
+    Otobus1= "06T9876"
+    Otobus2= "80TT458" 
+    Otobus3= "06RE876"
+    Otobus4= "34UY887"
+    Otobusler = [Otobus1,Otobus2,Otobus3,Otobus4]
+ 
 
     def bilet_sat(self):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
-        
+        otobus1=
     
     def bilet_iade(self):
         """Otobusteki dolu koltuk sayisini 1 azaltir"""
@@ -13,6 +18,3 @@ class Otobus:
     
     def durum_yaz(self):
         """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
-        
-
-
